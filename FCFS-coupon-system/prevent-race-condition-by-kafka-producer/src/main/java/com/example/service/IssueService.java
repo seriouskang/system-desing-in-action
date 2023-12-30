f4cb7ca07c1d10c330968b470606882297d97f1c
seriouskang <scdean110@gmail.com>
2023-12-30 22:08:16 +0900

    feat(prevent-race-condition-by-kafka-producer): 쿠폰 발급 여부 관련 service 로직 추가

--- FCFS-coupon-system/prevent-race-condition-by-kafka-producer/src/main/java/com/example/service/IssueService.java
+++ FCFS-coupon-system/prevent-race-condition-by-kafka-producer/src/main/java/com/example/service/IssueService.java
@@ -2,19 +2,29 @@ package com.example.service;
 
 import com.example.producer.CouponCreateProducer;
 import com.example.repository.CouponCountRepository;
+import com.example.repository.CouponIssuedUserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IssueService {
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final CouponIssuedUserRepository couponIssuedUserRepository;
 
-    public IssueService(CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public IssueService(CouponCountRepository couponCountRepository,
+                        CouponCreateProducer couponCreateProducer,
+                        CouponIssuedUserRepository couponIssuedUserRepository) {
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.couponIssuedUserRepository = couponIssuedUserRepository;
     }
 
     public void issue(Long userId) {
+        Long issued = couponIssuedUserRepository.add(userId);
+        if(issued != 1) {
+            return;
+        }
+
         long count = couponCountRepository.increment();
 
         if(count > 100) {
