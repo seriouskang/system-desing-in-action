4ad85886808a1934b6eb515140af2c21e215eff0
seriouskang <scdean110@gmail.com>
2023-12-30 19:32:44 +0900

    feat: prevent-race-condition-by-kafka-consumer 프로젝트 신규

---
+++ FCFS-coupon-system/prevent-race-condition-by-kafka-consumer/src/main/java/com/example/preventraceconditionbykafkaconsumer/PreventRaceConditionByKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.preventraceconditionbykafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreventRaceConditionByKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreventRaceConditionByKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ FCFS-coupon-system/prevent-race-condition-by-kafka-consumer/src/test/java/com/example/preventraceconditionbykafkaconsumer/PreventRaceConditionByKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preventraceconditionbykafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreventRaceConditionByKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
