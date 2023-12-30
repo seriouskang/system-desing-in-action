700e9ab0afc7372e9e6e460698f7c9264e466068
seriouskang <scdean110@gmail.com>
2023-12-30 18:55:27 +0900

    feat: producer 관련 기능 추가

--- FCFS-coupon-system/prevent-race-condition-by-kafka/src/main/java/com/example/api/service/IssueService.java
+++ FCFS-coupon-system/prevent-race-condition-by-kafka/src/main/java/com/example/api/service/IssueService.java
@@ -1,24 +1,29 @@
 package com.example.api.service;
 
-import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IssueService {
     private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
+    private final CouponCreateProducer couponCreateProducer;
 
-    public IssueService(CouponRepository couponRepository) {
+    public IssueService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
 
     public void issue(Long userId) {
-        long count = couponRepository.count();
+        long count = couponCountRepository.increment();
 
         if(count > 100) {
             return;
         }
 
-        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
     }
 }
