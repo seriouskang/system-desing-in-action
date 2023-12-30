77999e0ccaae6c0a0f27df71ec97b2c12dae3998
seriouskang <scdean110@gmail.com>
2023-12-30 19:59:08 +0900

    feat(prevent-race-condition-by-kafka-consumer): consumer config/consumer 추가

---
+++ FCFS-coupon-system/prevent-race-condition-by-kafka-consumer/src/main/java/com/example/consumer/CouponCreatedConsumer.java
@@ -0,0 +1,20 @@
+package com.example.consumer;
+
+import com.example.domain.Coupon;
+import com.example.repository.CouponRepository;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CouponCreatedConsumer {
+    private final CouponRepository couponRepository;
+
+    public CouponCreatedConsumer(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
+    }
+
+    @KafkaListener(topics = "coupon_create", groupId = "group_1")
+    public void listener(Long userId) {
+        couponRepository.save(new Coupon(userId));
+    }
+}
