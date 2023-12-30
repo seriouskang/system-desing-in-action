11d59be01cf3aa5e3d0ca00e36238141397966b9
seriouskang <scdean110@gmail.com>
2023-12-30 13:33:41 +0900

    test(쿠폰 발급): race condition 관련 테스트 코드 추가

--- FCFS-coupon-system/api/src/test/java/com/example/api/service/IssueServiceTest.java
+++ FCFS-coupon-system/api/src/test/java/com/example/api/service/IssueServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -23,5 +27,27 @@ class IssueServiceTest {
         // then
         long issuedCount = couponRepository.count();
         assertThat(issuedCount).isEqualTo(1);
+    }
+
+    @Test
+    void issue_many_time() throws InterruptedException {
+        int issueAttemptCount = 1_000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(issueAttemptCount);
+
+        for(int i=0; i<issueAttemptCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    issueService.issue(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        long issueCount = couponRepository.count();
+        assertThat(issueCount).isEqualTo(100);
     }
 }
