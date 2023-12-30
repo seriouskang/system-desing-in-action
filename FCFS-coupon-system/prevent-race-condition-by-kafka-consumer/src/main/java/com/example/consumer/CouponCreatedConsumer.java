ce8faf1b347f9a980534b53a15d380d8b7612324
seriouskang <scdean110@gmail.com>
2023-12-30 22:35:28 +0900

    feat(prevent-race-condition-by-kafka-consumer): 쿠폰 발급 시 에러 관련 consumer 로직 추가

--- FCFS-coupon-system/prevent-race-condition-by-kafka-consumer/src/main/java/com/example/consumer/CouponCreatedConsumer.java
+++ FCFS-coupon-system/prevent-race-condition-by-kafka-consumer/src/main/java/com/example/consumer/CouponCreatedConsumer.java
@@ -1,20 +1,32 @@
 package com.example.consumer;
 
 import com.example.domain.Coupon;
+import com.example.domain.FailedEvent;
 import com.example.repository.CouponRepository;
+import com.example.repository.FailedEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CouponCreatedConsumer {
+    private final Logger logger = LoggerFactory.getLogger(CouponCreatedConsumer.class);
     private final CouponRepository couponRepository;
+    private final FailedEventRepository failedEventRepository;
 
-    public CouponCreatedConsumer(CouponRepository couponRepository) {
+    public CouponCreatedConsumer(CouponRepository couponRepository, FailedEventRepository failedEventRepository) {
         this.couponRepository = couponRepository;
+        this.failedEventRepository = failedEventRepository;
     }
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
-        couponRepository.save(new Coupon(userId));
+        try {
+            couponRepository.save(new Coupon(userId));
+        } catch (Exception e) {
+            logger.error("failed to issue coupon: {}", userId);
+            failedEventRepository.save(new FailedEvent(userId));
+        }
     }
 }
